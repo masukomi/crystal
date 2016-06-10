b841f9077fc0b921853de0c463bce4d3e8f91e35
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-06-10 14:29:29 -0300

    GIve syntax error when using a local variable inside an assignment to that same local variable

--- src/compiler/crystal/syntax/parser.cr
+++ src/compiler/crystal/syntax/parser.cr
@@ -36,6 +36,7 @@ module Crystal
       @wants_doc = false
       @doc_enabled = false
       @no_type_declaration = 0
+      @assigned_vars = [] of String
     end
 
     def wants_doc=(wants_doc)
@@ -339,7 +340,13 @@ module Crystal
               atomic = UninitializedVar.new(atomic, type).at(location)
               return atomic
             else
-              value = parse_op_assign_no_control
+              if atomic.is_a?(Var) && !var?(atomic.name)
+                @assigned_vars.push atomic.name
+                value = parse_op_assign_no_control
+                @assigned_vars.pop
+              else
+                value = parse_op_assign_no_control
+              end
             end
 
             pop_def if needs_new_scope
@@ -3626,6 +3633,10 @@ module Crystal
               end
               Var.new name
             else
+              if !force_call && !block_arg && !named_args && !global && !last_call_has_parentheses && @assigned_vars.includes?(name)
+                raise "read before definition of local variable '#{name}'", location
+              end
+
               Call.new nil, name, [] of ASTNode, nil, block_arg, named_args, global, name_column_number, last_call_has_parentheses
             end
           end
